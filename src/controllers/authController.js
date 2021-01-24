21f93c4f3ddcfe513ff1e749a1c6353fb2ae1b24
orlando-messias <programadorlando@gmail.com>
2021-01-24 20:08:13 -0300

    nodemailer implementation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,9 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const { createToken } = require('../middlewares/auth');
+const mailer = require('../modules/mailer');
 
 // register a new user
 const registerUser = async (req, res) => {
@@ -20,14 +22,12 @@ const registerUser = async (req, res) => {
     return res.status(201).send({ user, token: createToken(user) });
 
   } catch (err) {
-    console.log(err)
     return res.status(400).send({ error: 'Registration failed' });
   }
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, ' ', password)
   try {
     const user = await User.findOne({ email }).select('+password');
 
@@ -46,7 +46,71 @@ const login = async (req, res) => {
 
 };
 
+const forgot_password = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send({ error: 'User not found' });
+
+    const token = crypto.randomBytes(20).toString('hex');
+
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await User.findByIdAndUpdate(user._id, {
+      '$set': {
+        passwordResetToken: token,
+        passwordResetExpires: now
+      }
+    });
+
+    mailer.sendMail({
+      to: email,
+      from: 'orlando@gmail.com',
+      template: 'auth/forgot',
+      context: { token }
+    }, (err) => {
+      if (err) return res.status(400).send({ error: 'Cannot send forgot password email' });
+
+      return res.send();
+    });
+
+  } catch (error) {
+    res.status(400).json({ error: 'Error on forgot password.' });
+  }
+};
+
+const reset_password = async (req, res) => {
+  const { email, token, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email })
+      .select('+passwordResetToken passwordResetExpires');
+
+    if (!user) return res.status(400).send({ error: 'User not found' });
+
+    if (token !== user.passwordResetToken)
+      res.status(400).json({ error: 'Invalid token' });
+
+    const now = new Date();
+    if (now > user.passwordResetExpires)
+      res.status(400).json({ error: 'Token expired, generate a new one' });
+    
+    user.password = password;
+
+    await user.save();
+
+    res.send();
+
+  } catch (error) {
+    res.status(400).json({ error: 'Error on reseting password.' });
+  }
+};
+
 module.exports = {
   registerUser,
-  login
+  login,
+  forgot_password,
+  reset_password
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,7 @@ const authController = require('../controllers/authController');
 
 authRoutes.post('/register', authController.registerUser);
 authRoutes.post('/login', authController.login);
+authRoutes.post('/forgot_password', authController.forgot_password);
+authRoutes.post('/reset_password', authController.reset_password);
 
 module.exports = authRoutes;
